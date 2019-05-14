72290d1afa4b86c2491dedc01971c22f381e78ae
dipankar-datta <dattapro5@gmail.com>
2019-05-14 21:11:59 +0530

    Spring Profiles

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingServiceImpl.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("de")
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryGermanGreetingServiceImpl implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "XXX---> Hello PrimaryGreetingService";
+        return "XXX---> Hallo German PrimaryGermanGreetingServiceImpl";
     }
 }
--- src/main/java/guru/springframework/services/PrimarySpanishGreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingServiceImpl.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("es")
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingServiceImpl implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "XXX---> Hello PrimaryGreetingService";
+        return "XXX---> Ola Spanish PrimarySpanishGreetingServiceImpl";
     }
 }
